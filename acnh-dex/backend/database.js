fc834a2b15ce201b4dc0598a9eca4a34960e3e74
Ariel Turner <39006828+arielturner@users.noreply.github.com>
2020-11-13 11:11:33 -0500

    Delete database.js

--- acnh-dex/backend/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const connection = '';
-mongoose.connect(
-  connection, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
-)
-  .then(() => console.log('Database Connected Successfully'))
-  .catch((err) => console.log(err));
