3e1874b89abfde54c4da92235880b12871c29f75
Ariel Turner <arieltrnr@gmail.com>
2020-11-21 12:52:52 -0500

    move theme provider up a level

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,61 +1,19 @@
 import './app.scss';
 import React from 'react';
-import { teal, lightBlue } from '@material-ui/core/colors';
 import { Switch, Route } from 'react-router-dom';
-import InvertColorsIcon from '@material-ui/icons/InvertColors';
-import {
-  IconButton, Typography, Toolbar, CssBaseline, AppBar, createMuiTheme, ThemeProvider, Tooltip,
-} from '@material-ui/core';
+import { Toolbar } from '@material-ui/core';
 import Welcome from '../welcome/welcome';
 import Home from '../home/home';
 
-const lightTheme = createMuiTheme({
-  palette: {
-    primary: teal,
-    secondary: lightBlue,
-    type: 'light',
-  },
-});
-const darkTheme = createMuiTheme({
-  palette: {
-    primary: teal,
-    secondary: lightBlue,
-    type: 'dark',
-  },
-});
-
 function App() {
-  const [theme, setTheme] = React.useState(localStorage.getItem('theme') === 'dark' ? darkTheme : lightTheme);
-
-  const handleClick = () => {
-    setTheme(theme === darkTheme ? lightTheme : darkTheme);
-    localStorage.setItem('theme', theme.palette.type);
-  };
-
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AppBar position="fixed" className="app-bar">
-        <Toolbar>
-          <Typography variant="h6" noWrap>
-            ACNH Dex
-          </Typography>
-          <Tooltip title="Toggle theme">
-            <IconButton style={{ color: 'white' }} onClick={handleClick}>
-              <InvertColorsIcon />
-            </IconButton>
-          </Tooltip>
-        </Toolbar>
-      </AppBar>
-
-      <main>
-        <Toolbar />
-        <Switch>
-          <Route exact path="/" component={Welcome} />
-          <Route path="/:id" component={Home} />
-        </Switch>
-      </main>
-    </ThemeProvider>
+    <main>
+      <Toolbar />
+      <Switch>
+        <Route exact path="/" component={Welcome} />
+        <Route path="/:id" component={Home} />
+      </Switch>
+    </main>
   );
 }
 
--- src/global/global-context.jsx
+++ src/global/global-context.jsx
@@ -1,15 +1,41 @@
 import './global-context.scss';
 import React from 'react';
 import PropTypes from 'prop-types';
-import { IconButton, Snackbar, CircularProgress } from '@material-ui/core';
+import { teal, lightBlue } from '@material-ui/core/colors';
+import InvertColorsIcon from '@material-ui/icons/InvertColors';
+import {
+  IconButton, Snackbar, CircularProgress, ThemeProvider, AppBar, CssBaseline, createMuiTheme,
+  Toolbar, Tooltip, Typography,
+} from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 
 export const GlobalContext = React.createContext();
 
+const lightTheme = createMuiTheme({
+  palette: {
+    primary: teal,
+    secondary: lightBlue,
+    type: 'light',
+  },
+});
+const darkTheme = createMuiTheme({
+  palette: {
+    primary: teal,
+    secondary: lightBlue,
+    type: 'dark',
+  },
+});
+
 function GlobalContextProvider({ children }) {
+  const [theme, setTheme] = React.useState(localStorage.getItem('theme') === 'dark' ? darkTheme : lightTheme);
   const [loading, setLoading] = React.useState(false);
   const [open, setOpen] = React.useState(false);
   const [message, setMessage] = React.useState('');
+
+  const handleThemeClick = () => {
+    setTheme(theme === darkTheme ? lightTheme : darkTheme);
+    localStorage.setItem('theme', theme.palette.type);
+  };
 
   const toggleLoadingSpinner = (value) => {
     setLoading(value);
@@ -25,7 +51,7 @@ function GlobalContextProvider({ children }) {
     setSnackbarMessage,
   });
 
-  const handleClose = (event, reason) => {
+  const handleSnackbarClose = (event, reason) => {
     if (reason !== 'clickaway') {
       setOpen(false);
     }
@@ -33,31 +59,46 @@ function GlobalContextProvider({ children }) {
 
   return (
     <GlobalContext.Provider value={context}>
-      {children}
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="fixed" className="app-bar">
+          <Toolbar>
+            <Typography variant="h6" noWrap>
+              ACNH Dex
+            </Typography>
+            <Tooltip title="Toggle theme">
+              <IconButton style={{ color: 'white' }} onClick={handleThemeClick}>
+                <InvertColorsIcon />
+              </IconButton>
+            </Tooltip>
+          </Toolbar>
+        </AppBar>
+        {children}
 
-      {loading
-        ? (
-          <div className="spinner-container">
-            <CircularProgress color="secondary" />
-          </div>
-        )
-        : null}
+        {loading
+          ? (
+            <div className="spinner-container">
+              <CircularProgress color="secondary" />
+            </div>
+          )
+          : null}
 
-      <Snackbar
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-        open={open}
-        autoHideDuration={6000}
-        onClose={handleClose}
-        message={message}
-        action={
-          <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
-            <CloseIcon fontSize="small" />
-          </IconButton>
-        }
-      />
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+          }}
+          open={open}
+          autoHideDuration={6000}
+          onClose={handleSnackbarClose}
+          message={message}
+          action={(
+            <IconButton size="small" aria-label="close" color="inherit" onClick={handleSnackbarClose}>
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          )}
+        />
+      </ThemeProvider>
     </GlobalContext.Provider>
   );
 }
